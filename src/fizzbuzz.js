da80701447a9dad7d91cd6040b705d731e2ae5bd
pamela-arriaga <pamelav.arriaga@gmail.com>
2026-05-09 00:15:01 -0600

    Realice el código donde los multiplos de 3 serán reemplazados por la palabra Fizz, los multiplos de 5 por la palabra Buzz y multiplos de 3 y5 por la palabra FizzBuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,13 @@
 export function fizzbuzz(){
-  //tu código va aqui
- 
-  
+  for (let i = 1; i <= 100; i ++){ //Contador del 1 al 100
+    if (i % 3 === 0 && i % 5 === 0){ //Multiplos de 3 AND 5
+      console.log("FizzBuzz")} //Imprime la palabra FizzBuzz para multiplos en comun de 3 y 5
+    else if (i % 3 === 0){ //Multiplos de 3
+      console.log("Fizz")}
+    else if (i % 5 === 0){ //Multiplos de 5
+      console.log("Buzz")}
+    else{
+      console.log(i)} //Imprime los números restantes que no sean multiplos
+    }
 }
-
 fizzbuzz();
